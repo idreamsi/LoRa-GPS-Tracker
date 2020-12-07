23d693ebea48a865eb91a65100882809fd7bb1c6
Infinite Dreams <8011806+idreamsi@users.noreply.github.com>
2020-12-07 12:17:47 +0330

    Update Sender.ino

--- Sender.ino
+++ Sender.ino
@@ -3,7 +3,6 @@
  * LoRa GPS Tracker by Wio Terminal
  * Live tracking on OpenStreetMap with GPS over LoRa
  * Please find more details at instructables:
- * Please find more details at instructables:
  * https://www.hackster.io/idreams/lora-gps-tracker-by-wio-terminal-5d8647
  * 
  ******************************************************************************/
